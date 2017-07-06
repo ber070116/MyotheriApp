b88a2aedb21a0c8a1fe11c9539845fff1a4bad72
ber070116 <brandomhk@gmail.com>
2017-07-06 00:09:28 -0400

    Mejoras y casi conexion via json lista

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -9,3 +9,24 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
+
+// funcion para logearse
+function login(){
+  var nombre_usuario = document.querySelector('#form_usuario').value;
+  var contrasena = document.querySelector('#form_contrasena').value;
+  
+  var url = 'http://myotheri-brandomhk434243.codeanyapp.com:8000/test/api-token-auth/';
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
+  xhttp.open('POST', url, true);
+  xhttp.setRequestHeader('Content-type', 'application/json');
+  var datos = new FormData();
+  datos.append('nombre_usuario', nombre_usuario);
+  datos.append('password', contrasena);
+  alert(datos);
+  xhttp.send(datos);
+}
